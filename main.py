4e652c808171f6dbd03570b6c50ede9f3f4ea0ac
prashantbh <bhuskutep@gmail.com>
2020-08-26 07:30:50 +0000

    added pop method.

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ basket= ['a', 'b', 'e','x', 'd']
 basket.append('p')
 new_basket= basket[:]
 new_basket.sort()
+print(basket)
+print(new_basket)
 basket.insert(1,'R')
 basket.remove('d')
+new_basket.pop()
+basket.pop()
 print(basket)
 print(new_basket)
